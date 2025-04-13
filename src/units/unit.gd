182ab03f02f4c6c69c1b8e0bc8157eafbf3853a5
Jakub Szaredko <jakub.szaredko@gmail.com>
2025-04-13 11:14:28 +0200

    Refactor: Use map position instead of world position in 'Unit'

--- src/units/unit.gd
+++ src/units/unit.gd
@@ -5,13 +5,14 @@ class_name Unit
 @export_range(1, 1024) var stamina: int
 @export var offset: Vector2
 
-var offset_position: Vector2
+var map_position: Vector2i
+var board: Board
 
 
-func _ready() -> void:
-	offset_position = position - offset
+func set_position_from_map(map: Vector2i) -> void:
+	map_position = map
+	position = board.map_to_local(map_position) + offset
 
 
-func move(offset: Vector2) -> void:
-	offset_position = offset
-	position = offset_position + self.offset
+func move(to: Vector2i) -> void:
+	set_position_from_map(to)
