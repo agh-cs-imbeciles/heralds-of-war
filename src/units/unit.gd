457443833d70a3cc5613d3f5e38b9fa1a0f4825c
Jakub Szaredko <jakub.szaredko@gmail.com>
2025-04-13 11:14:28 +0200

    Feat: Limit unit movement range by stamina

--- src/units/unit.gd
+++ src/units/unit.gd
@@ -9,6 +9,30 @@ var map_position: Vector2i
 var board: Board
 
 
+func can_move(map_index: Vector2i) -> bool:
+	var legal_moves = get_legal_moves(map_position)
+
+	for move in legal_moves:
+		if map_index == move:
+			return true
+
+	return false
+
+
+func get_legal_moves(map_index: Vector2i) -> Array[Vector2i]:
+	var legal_moves: Array[Vector2i] = []
+
+	for cell in board.get_used_cells():
+		var s = board.get_cell_id(map_index)
+		var t = board.get_cell_id(cell)
+		var path = board.path_finder.get_point_path(s, t)
+
+		if 1 < path.size() and path.size() <= stamina:
+			legal_moves.append(cell)
+
+	return legal_moves
+
+
 func set_position_from_map(map: Vector2i) -> void:
 	map_position = map
 	position = board.map_to_local(map_position) + offset
