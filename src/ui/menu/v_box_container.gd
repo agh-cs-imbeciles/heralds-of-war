ba95149ced36ba90716d0eabd691bc2f4af5ffd4
kpiotr6 <piotrek@komlan.pl>
2025-04-15 23:05:36 +0200

    Add requested changes

--- src/ui/menu/v_box_container.gd
+++ src/ui/menu/v_box_container.gd
@@ -1,7 +1,16 @@
 extends VBoxContainer
 
+var control_children: Array[Control]
 
+
+func _ready() -> void:
+	control_children.assign(get_children()
+		.filter(func (child): return child is Control))
+	print(control_children)
+	
+	
 func _on_resized() -> void:
-	var height = get_parent_area_size()[1]
-	for child: Control in get_children():
-		child.add_theme_font_size_override("font_size", height/4)
+	var height = get_parent_area_size().y
+	
+	for control in control_children:
+		control.add_theme_font_size_override("font_size", height / 4)
