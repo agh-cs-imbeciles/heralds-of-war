606a15c629531b96b01c20950bfc51129b1e9dac
Jakub Szaredko <jakub.szaredko@gmail.com>
2025-05-04 19:16:49 +0200

    Feat: Add weights to individual tiles (#25)

--- src/board/board.gd
+++ src/board/board.gd
@@ -7,6 +7,8 @@ enum HighlightTile { HOVER, FOCUS, MOVABLE }
 var is_tile_focused: bool = false
 var moves: Array[Vector2i] = []
 
+var cost: Dictionary = {}
+
 var path_finder: AStar2D = AStar2D.new()
 
 var swordsman_scene: PackedScene = preload("res://scenes/units/swordsman.tscn")
@@ -21,6 +23,8 @@ var swordsman: Unit
 
 
 func _ready() -> void:
+	load_board_cost()
+
 	init_path_finder()
 
 	instantiate_swordsman()
@@ -28,11 +32,28 @@ func _ready() -> void:
 	instantiate_highlight_tile(HighlightTile.FOCUS)
 
 
+func load_board_cost() -> void:
+	var file = FileAccess.open(
+		"res://assets/resources/board_cost.json",
+		FileAccess.READ
+	)
+	var parsed_file = JSON.parse_string(file.get_as_text())
+
+	for atlas_coords in parsed_file:
+		if typeof(parsed_file[atlas_coords]) != TYPE_STRING:
+			continue
+		if parsed_file[atlas_coords] != "inf":
+			continue
+
+		parsed_file[atlas_coords] = 9223372036854775807  # 2^63 - 1
+
+	cost = parsed_file
+
+
 func init_path_finder() -> void:
 	for cell in get_used_cells():
 		var i = get_cell_id(cell)
-		path_finder.add_point(i, cell)
-
+		path_finder.add_point(i, cell, get_cell_cost(cell))
 	for cell in get_used_cells():
 		var i = get_cell_id(cell)
 		for surrounding_cell in get_surrounding_cells(cell):
@@ -45,7 +66,7 @@ func init_path_finder() -> void:
 func instantiate_swordsman() -> Unit:
 	swordsman = swordsman_scene.instantiate()
 
-	swordsman.stamina = 6
+	swordsman.stamina = 60
 	swordsman.offset = Vector2(8, -20)
 	swordsman.board = self
 	swordsman.set_position_from_map(get_used_rect().size / 2)
@@ -107,6 +128,12 @@ func _input(event: InputEvent) -> void:
 		elif event.button_index == MOUSE_BUTTON_RIGHT:
 			if is_tile_focused:
 				on_unfocus_cell(mouse_map_position, false)
+
+
+func get_cell_cost(map_index: Vector2i) -> int:
+	var atlas_coords = get_cell_atlas_coords(map_index)
+	var coord_key = "%s,%s" % [atlas_coords.x, atlas_coords.y]
+	return cost.get(coord_key, 9223372036854775807)
 
 
 func get_cell_id(map_index: Vector2i) -> int:
