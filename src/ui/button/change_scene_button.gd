7fda6a18ca2f3ffeb91d1b11b25e7d9925e9d22b
Jakub Szaredko <jakub.szaredko@gmail.com>
2025-05-29 20:06:18 +0200

    Refactor: Change pressed event handling (#54)

--- src/ui/button/change_scene_button.gd
+++ src/ui/button/change_scene_button.gd
@@ -3,5 +3,5 @@ class_name ChangeSceneButton extends StandardButton
 @export var scene_to_change: PackedScene
 
 
-func __change_scene() -> void:
+func _pressed() -> void:
 	UiUtils.change_scene(self, scene_to_change)
