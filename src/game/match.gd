c404c7b239ae8233a5c186749a24e72da4a6f0d9
Jakub Szaredko <jakub.szaredko@gmail.com>
2025-05-31 17:18:16 +0200

    Feat: Add ending of match This feature fully adds game ending logic. The win condition is determined on every unit death, if player's unit was last, then the match is over, other player is victorious (note: works only for 2-player gameplay). It is worth mentioning that interfaces for pausing and resuming a match are implemented.
    New files are introduced:
    - end_game_modal.tscn — end-of-game modal, which displays which player won the
      match and allows you to go back to the match selection scene
    - match_ui.gd (`MatchUi`) — core of Match UI
    - darkened_blur.gdshader — shader for darkening and blurring a background (based
      on a great shader: https://godotshaders.com/shader/darkened-blur/)

--- src/game/match.gd
+++ src/game/match.gd
@@ -3,6 +3,7 @@ class_name Match extends Node2D
 enum Phase { INIT = 0, PLACEMENT = 1, PLAY = 2 }
 
 signal turn_ended(turn: int)
+signal ended(victor: String)
 
 @export var match_name: String
 @export var unit_count_per_player: int = 3
@@ -18,17 +19,28 @@ var play_manager: MatchPlayManager
 
 
 func _ready() -> void:
-	phase_manager = MatchPhaseManager.new(self)
-	phase_manager.phase_changed.connect(__on_phase_changed)
+	# Making sure that every match is not paused, for instance the end of the
+	# previous match pauses the whole game globally.
+	resume()
 
+	phase_manager = MatchPhaseManager.new(self)
 	placement_manager = MatchPlacementManager.new(self)
 	play_manager = MatchPlayManager.new(self)
+
+	phase_manager.phase_changed.connect(__on_phase_changed)
+	play_manager.ended.connect(__on_match_ended)
 
 	phase_manager.enter_phase(Phase.PLACEMENT)
 
 
 func __on_phase_changed(new_phase: Phase) -> void:
 	phase = new_phase
+
+
+func __on_match_ended(victor: String) -> void:
+	print("The match has ended")
+	print("Player %s is victorious" % victor)
+	ended.emit(victor)
 
 
 func get_current_player() -> String:
@@ -39,3 +51,11 @@ func end_turn() -> void:
 	print("Turn %d has been finished" % turn)
 	turn += 1
 	turn_ended.emit(turn)
+
+
+func pause() -> void:
+	get_tree().paused = true
+
+
+func resume() -> void:
+	get_tree().paused = false
