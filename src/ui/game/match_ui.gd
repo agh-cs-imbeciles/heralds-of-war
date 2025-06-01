3b5ad4d116d498856a9cae153908c61c4e662e39
Jakub Szaredko <jakub.szaredko@gmail.com>
2025-06-01 13:38:06 +0200

    Feat: Add ending of match (#60)
    * Feat: Add ending of match
    This feature fully adds game ending logic. The win condition is determined on
    every unit death, if player's unit was last, then the match is over, other
    player is victorious (note: works only for 2-player gameplay). It is worth
    mentioning that interfaces for pausing and resuming a match are implemented.
    
    New files are introduced:
    - end_game_modal.tscn — end-of-game modal, which displays which player won the
      match and allows you to go back to the match selection scene
    - match_ui.gd (`MatchUi`) — core of Match UI
    - darkened_blur.gdshader — shader for darkening and blurring a background (based
      on a great shader: https://godotshaders.com/shader/darkened-blur/)

---
+++ src/ui/game/match_ui.gd
@@ -0,0 +1,23 @@
+class_name MatchUi extends Node
+
+@onready var __match: Match = $".."
+@onready var __board_ui: BoardUi = $"BoardUi"
+@onready var end_game_panel: Panel = $"UiCanvasLayer/EndGamePanel"
+
+
+func _ready() -> void:
+	__match.match_ended.connect(__on_match_ended)
+
+
+func __on_match_ended(victor: String) -> void:
+	show_end_game_panel(victor)
+
+
+func show_end_game_panel(victor: String) -> void:
+	__match.pause()
+	__board_ui.hide()
+
+	var label: Label = end_game_panel.find_child("VictorLabel")
+	label.text = label.text % victor
+
+	end_game_panel.show()
