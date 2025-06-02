c37e70a205a1cadcb7c4f28a6d3f0bcc8ad57c6e
MatoKnap <mknap@student.agh.edu.pl>
2025-06-02 21:10:01 +0200

    Fixed comments

--- src/globals.gd
+++ src/globals.gd
@@ -2,9 +2,9 @@ extends Node
 
 const CELL_COST_INFINITY = 16777215  # 2^24 - 1
 
-const PLAYER_A_COLOR = Color(1, 0.8, 0.8) # Czerwony dla gracza A
-const PLAYER_B_COLOR = Color(0.8, 0.8, 1) # Niebieski dla gracza B
-const PLAYER_TEXT_COLOR_CURRENT = Color.WHITE # Kolor tekstu dla aktywnego gracza w kolejce
-const PLAYER_TEXT_COLOR_NORMAL = Color.LIGHT_GRAY # Kolor tekstu dla nieaktywnych graczy w kolejce
-const PLAYER_TEXT_BG_CURRENT = Color(0.9, 0.9, 0.1, 0.6) # Lekko żółtawe tło dla aktywnego
-const PLAYER_TEXT_BG_NORMAL = Color(0.5, 0.5, 0.5, 0.4) # Szarawe tło dla nieaktywnych
+const PLAYER_A_COLOR = Color(1, 0.8, 0.8)
+const PLAYER_B_COLOR = Color(0.8, 0.8, 1)
+const PLAYER_TEXT_COLOR_CURRENT = Color.WHITE # Text color for the active player in the queue
+const PLAYER_TEXT_COLOR_NORMAL = Color.LIGHT_GRAY # Text color for inactive players in the queue
+const PLAYER_TEXT_BG_CURRENT = Color(0.9, 0.9, 0.1, 0.6) # Background color for the active player in the queue
+const PLAYER_TEXT_BG_NORMAL = Color(0.5, 0.5, 0.5, 0.4) # Background color for inactive players in the queue
